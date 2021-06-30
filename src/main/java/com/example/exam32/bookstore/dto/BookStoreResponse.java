d046b821ae38b0ce3a448d53ae4d6301423317f3
Taitana <taitana.ym@outlook.com>
2021-06-30 15:37:00 +0700

    add controller testing & service testing

--- src/main/java/com/example/exam32/bookstore/dto/BookStoreResponse.java
+++ src/main/java/com/example/exam32/bookstore/dto/BookStoreResponse.java
@@ -1,5 +1,7 @@
 package com.example.exam32.bookstore.dto;
 
+import java.util.Objects;
+
 public class BookStoreResponse{
 	private int amount;
 	private int price;
@@ -11,6 +13,22 @@ public class BookStoreResponse{
 		this.price = price;
 		this.id = id;
 		this.bookName = bookName;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof BookStoreResponse)) return false;
+		BookStoreResponse that = (BookStoreResponse) o;
+		return getAmount() == that.getAmount() &&
+				getPrice() == that.getPrice() &&
+				getId() == that.getId() &&
+				Objects.equals(getBookName(), that.getBookName());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getAmount(), getPrice(), getId(), getBookName());
 	}
 
 	public BookStoreResponse() {
--- src/test/java/com/example/exam32/bookstore/BookStoreControllerTest.java
+++ src/test/java/com/example/exam32/bookstore/BookStoreControllerTest.java
@@ -1,7 +1,43 @@
 package com.example.exam32.bookstore;
 
+import com.example.exam32.bookstore.dto.BookStoreRequest;
+import com.example.exam32.bookstore.dto.BookStoreResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookStoreControllerTest {
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Autowired
+    BookStoreRepository bookStoreRepository;
+
+    @Test
+    public void callAPIWithPathVariableAndDatabase(){
+
+//       ? Create Data
+        bookStoreRepository.save(new BookStoreEntity(10, 400, 1, "test"));
+
+//       ? Call API
+        BookStoreResponse response = restTemplate.getForObject("/books/1", BookStoreResponse.class);
+
+//       ? Testing Zone
+        BookStoreResponse expect = new BookStoreResponse(10, 400,1, "test");
+        assertEquals(expect, response);
+    }
+
+    @Test
+    public void callCreateEmployeeApi(){
+        BookStoreResponse expect = new BookStoreResponse(10, 400, 5, "test");
+        BookStoreRequest req = new BookStoreRequest(10, 400, "test");
+        BookStoreResponse response = restTemplate.postForObject("/books", req,  BookStoreResponse.class);
+        assertEquals(10, response.getAmount());
+        assertEquals(400, response.getPrice());
+        assertEquals("test", response.getBookName());
+    }
 }
