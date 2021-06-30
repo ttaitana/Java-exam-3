4c02f60735ced675329b6f3487091a413877bc5c
Taitana <taitana.ym@outlook.com>
2021-06-30 14:11:45 +0700

    init project

---
+++ src/main/java/com/example/exam32/Exam32Application.java
@@ -0,0 +1,13 @@
+package com.example.exam32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exam32Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exam32/Exam32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
